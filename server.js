76507f16edd9984bd8d2381f730bf08cbc8fcfaf
Sean Pheneger <wcolts2000@gmail.com>
2018-09-24 15:56:22 -0700

    updating db

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-metric-22388',
     user : 'postgres',
     password : 'Oseanasee69!',
     database : 'smart-brain'
