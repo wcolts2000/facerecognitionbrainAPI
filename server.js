be77d0a4c34ce330aea787353b1498ad51d109b6
Sean Pheneger <wcolts2000@gmail.com>
2018-09-24 13:47:09 -0700

    changing db settings

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('it is working') })
 app.post('/signin', signin.handleSignin(knex, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, knex, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, knex) })
